056612449b985eaa3e14ffc5da5a72d28e1dd374
mathimalar2005 <mathimalarsankar@gmail.com>
2025-12-20 19:52:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
+
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-    cout << "Hello GitHub from C++!" << endl;
+    int guess, number;
+    int count = 0;
+
+    srand(time(0));                 // random seed
+    number = rand() % 100 + 1;       // number between 1 and 100
+
+    cout << "Guess a number between 1 and 100\n";
+
+    while (true) {
+        cin >> guess;
+        count++;
+
+        if (guess < number) {
+            cout << "Guess a greater number\n";
+        } 
+        else if (guess > number) {
+            cout << "Guess a smaller number\n";
+        } 
+        else {
+            cout << "You are correct!\n";
+            break;
+        }
+    }
+
+    cout << "You took " << count << " chances.\n";
     return 0;
 }
